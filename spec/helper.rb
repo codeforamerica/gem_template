d035c217152d6432c98bfbcd65ae655ed9474729
Erik Michaels-Ober <sferik@gmail.com>
2011-05-29 10:14:26 -0700

    Push spec and lib onto the

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,4 +1,6 @@
+$:.unshift File.expand_path('..', __FILE__)
+$:.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
 SimpleCov.start
-require 'rspec'
 require 'gem_template'
+require 'rspec'
